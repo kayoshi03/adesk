c2e7caa8e3d5c38b1e4ff9953f452ac150fe85ba
Kayoshi <r0n1ronov1@gmail.com>
2023-11-05 21:03:47 +0300

    finish

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-import {Show} from "./module/show.js"
+import {Show, ShowFooter, ShowNavigate} from "./module/show.js"
 
 const menu = document.getElementById("menu")
 const menuList = document.getElementById("menuList")
+const children = document.querySelector(".list").children
+const footerNavigate = document.querySelector(".columns").children
+
+
+ShowFooter(footerNavigate)
+ShowNavigate(children)
 Show(menu, menuList)
--- module/show.js
+++ module/show.js
@@ -3,3 +3,47 @@ export const Show = (el, list) => {
         list.classList.toggle("show")
     })
 }
+
+export const ShowNavigate = (children) => {
+    for (let i = 0; i < children.length; i++) {
+        let button= children[i].children
+        if (button.length === 1) {
+            children[i].addEventListener("click", function () {
+                button[0].classList.toggle("rotate")
+            })
+        }
+        if (button.length > 1) {
+            children[i - 1].addEventListener("click", function () {
+                children[i].classList.toggle("showNav")
+            })
+        }
+    }
+}
+
+export const ShowFooter = (children) => {
+
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].children.length < 3) {
+            children[i].children[0].addEventListener("click", function () {
+                children[i].children[0].children[0].classList.toggle("rotate")
+                children[i].children[1].classList.toggle("showFooter")
+            })
+        }
+        else {
+            for(let j = 2; j < children[i].children.length; j++){
+                children[i].children[1].addEventListener("click", function () {
+                    children[i].children[j].classList.toggle("showFooter")
+                })
+            }
+            children[i].children[0].children[0].addEventListener("click", function () {
+                children[i].children[0].children[0].children[0].classList.toggle("rotate")
+                children[i].children[0].children[1].classList.toggle("showFooter")
+            })
+            children[i].children[1].addEventListener("click", function () {
+                children[i].children[1].children[0].classList.toggle("rotate")
+            })
+
+        }
+
+    }
+}
